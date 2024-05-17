1bcec4acc08237bae59795bd21c1cbd5a095296e
vivianvg <info.vivianguo@gmail.com>
2024-05-16 21:30:12 -0400

    initial model set up

--- cardtoon/cards/models.py
+++ cardtoon/cards/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Models to define:
+
+- message
+    - card (horizontal view?)
+    - letter (vertical view)
+- 
+
+questions:
+* should we also allow vertical for cards?
+* upload image -> upload 1 or multiple?
+
+
+other:
+* maybe for now use local storage
+
+"""
+
+class Message(models.Model):
+    # super class to Card & Letter
+
+    date_recieved = models.DateField()
+    date_opened = models.DateField()
+
+    sender = models.CharField(max_length=100)
+    receiver = models.CharField(max_length=100) # may not be neccesary
+
+    content = models.TextField()
+
+    orientation = None
+
+    class Meta:
+        abstract = True
+
+class Card(Message):
+
+    image = models.ImageField() 
+
+
+    pass
+
+class Letter(Message):
+
+
+    pass
+
